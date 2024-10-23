c19ca2816d8c72984cba8fc3588120b2221581e4
Sheng Yao <45750387+MuTangShawn@users.noreply.github.com>
2024-10-23 17:06:00 +0800

    Add files via upload

---
+++ usingexample.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+import SRU_Layer_tf291
+
+inputs = np.random.random((32, 10, 8))
+sru = SRU_Layer_tf291.SRU(4)
+output = sru(inputs)
+print(output.shape)
+sru = SRU_Layer_tf291.SRU(4, return_sequences=True, return_state=True)
+whole_sequence_output, final_state = sru(inputs)
+print(whole_sequence_output.shape)
+print(final_state.shape)
+bisru = tf.keras.layers.Bidirectional(SRU_Layer_tf291.SRU(4, return_sequences=True), merge_mode="concat")
+output = bisru(inputs)
+print(output.shape)
